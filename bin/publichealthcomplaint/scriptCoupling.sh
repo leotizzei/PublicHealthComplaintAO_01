09638f2d1860024023a5d4cf9216735afe8eccac
Andre Esteve <andreesteve@gmail.com>
2011-10-07 19:29:34 -0300

    Removed bin and tmp folders (they are created during building process) Classpath changed to avoid absolute references

--- bin/publichealthcomplaint/scriptCoupling.sh
+++
@@ -1,6 +0,0 @@
- #!/bin/bash
-	rm *.txt;
-        for i in $( ls ); do
-	    find $i -name "*.java" -or -name "*.aj" | xargs more | grep -E publichealthcomplaint. | grep -v publichealthcomplaint.$i | sort -nk 2 > $i.txt
-        done
-
